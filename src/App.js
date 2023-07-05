b67ab67a3d7148cacfb4a42ca19bd0be0beaabee
gerald <geraldmumo6@gmail.com>
2023-07-05 14:54:52 +0300

    audio commit

--- src/App.js
+++ src/App.js
@@ -2,68 +2,71 @@ import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
 
 const App = () => {
-  const [breakLength, setBreakLength] = useState(5);
-  const [sessionLength, setSessionLength] = useState(25);
-  const [timerLabel, setTimerLabel] = useState('Session');
-  const [timeLeft, setTimeLeft] = useState(sessionLength * 60);
-  const [isRunning, setIsRunning] = useState(false);
+ const [breakLength , setBreakLength] = useState(5);
+ const [sessionLength, setSessionLength] = useState(25);
+ const [timerLabel, setTimerlabel] = useState("Session");
+ const [timeLeft, setTimeLeft] = useState(sessionLength * 60);
+ const [isRunning, setIsRunning] = useState(false);
 
-  const audioRef = useRef(null);
+ //audio ref
+ const audioRef = useRef(null);
 
-  useEffect(() => {
-    setTimeLeft(sessionLength * 60);
-  }, [sessionLength]);
+ useEffect ( () => {
+  setTimeLeft(sessionLength * 60);
+ }, [sessionLength]);
 
-  useEffect(() => {
-    if (isRunning) {
-      const timer = setInterval(() => {
-        setTimeLeft((prevTimeLeft) => prevTimeLeft - 1);
-      }, 1000);
-
-      if (timeLeft === 0) {
-        audioRef.current.play();
-        if (timerLabel === 'Session') {
-          setTimerLabel('Break');
-          setTimeLeft(breakLength * 60);
-        } else {
-          setTimerLabel('Session');
-          setTimeLeft(sessionLength * 60);
-        }
+ useEffect( () => {
+  if(isRunning) {
+    const timer = setInterval( () => {
+      setTimeLeft((prevTimeLeft) => prevTimeLeft - 1);
+    }, 1000);
+    if (timeLeft === 0) {
+      audioRef.current.play();
+      setTimeout(() =>{
+        audioRef.current.pause();
+      }, 10000);
+      if(timerLabel === 'Session') {
+        setTimerlabel('Break');
+        setTimeLeft(breakLength * 60);
+      } else {
+        setTimerlabel('Session');
+        setTimeLeft(sessionLength * 60);
       }
-
-      return () => clearInterval(timer);
     }
-  }, [isRunning, timeLeft, breakLength, sessionLength, timerLabel]);
 
-  const formatTime = (timeInSeconds) => {
-    const minutes = Math.floor(timeInSeconds / 60)
-      .toString()
-      .padStart(2, '0');
-    const seconds = (timeInSeconds % 60).toString().padStart(2, '0');
-    return `${minutes}:${seconds}`;
-  };
+    return () => clearInterval(timer);
+  }
+ }, [isRunning, timeLeft, breakLength, sessionLength, timerLabel]);
 
-  const handleBreakDecrement = () => {
-    if (breakLength > 1 && !isRunning) {
+ //timeformat
+
+ const formatTime = (timeInSeconds) => {
+  const minutes = Math.floor(timeInSeconds / 60).toString().padStart(2, '0');
+  const seconds = (timeInSeconds % 60).toString().padStart(2, '0');
+  return `${minutes}:${seconds}`;
+ }
+ // handleeventscliks
+  const handleBreakDec = () => {
+    if(breakLength > 1 && !isRunning) {
       setBreakLength((prevBreakLength) => prevBreakLength - 1);
     }
   };
 
-  const handleBreakIncrement = () => {
-    if (breakLength < 60 && !isRunning) {
-      setBreakLength((prevBreakLength) => prevBreakLength + 1);
+  const handleBreakInc = () => {
+    if(breakLength < 60 && !isRunning) {
+      setBreakLength((prevBreakLength) => prevBreakLength -1);
     }
   };
 
-  const handleSessionDecrement = () => {
-    if (sessionLength > 1 && !isRunning) {
-      setSessionLength((prevSessionLength) => prevSessionLength - 1);
+  const handleSessionDec = () => {
+    if(sessionLength > 1 && !isRunning) {
+      setSessionLength((prevSessionLength) => prevSessionLength -1);
     }
   };
 
-  const handleSessionIncrement = () => {
-    if (sessionLength < 60 && !isRunning) {
-      setSessionLength((prevSessionLength) => prevSessionLength + 1);
+  const handleSessionInc = () => {
+    if(sessionLength < 60 && !isRunning) {
+      setSessionLength((prevSessionLength) => prevSessionLength -1);
     }
   };
 
@@ -72,14 +75,15 @@ const App = () => {
   };
 
   const handleReset = () => {
-    setIsRunning(false);
+    setBreakLength(false);
     setBreakLength(5);
     setSessionLength(25);
-    setTimerLabel('Session');
+    setTimerlabel('Session');
     setTimeLeft(25 * 60);
     audioRef.current.pause();
     audioRef.current.currentTime = 0;
-  };
+  }
+
 
   return (
     <div className="App">
@@ -90,7 +94,7 @@ const App = () => {
           <button
             id="break-decrement"
             className="control-button"
-            onClick={handleBreakDecrement}
+            onClick={handleBreakDec}
           >
             -
           </button>
@@ -100,7 +104,7 @@ const App = () => {
           <button
             id="break-increment"
             className="control-button"
-            onClick={handleBreakIncrement}
+            onClick={handleBreakInc}
           >
             +
           </button>
@@ -110,7 +114,7 @@ const App = () => {
           <button
             id="session-decrement"
             className="control-button"
-            onClick={handleSessionDecrement}
+            onClick={handleSessionDec}
           >
             -
           </button>
@@ -120,14 +124,14 @@ const App = () => {
           <button
             id="session-increment"
             className="control-button"
-            onClick={handleSessionIncrement}
+            onClick={handleSessionInc}
           >
             +
           </button>
         </div>
       </div>
       <div id="timer-label">{timerLabel}</div>
-      <div id="time-left" className={isRunning ? 'running' : ''}>
+      <div id="time-left" >
         {formatTime(timeLeft)}
       </div>
       <button id="start_stop" onClick={handleStartStop}>
@@ -136,7 +140,7 @@ const App = () => {
       <button id="reset" onClick={handleReset}>
         Reset
       </button>
-      <audio id="beep" ref={audioRef} src="beep.mp3" />
+      <audio id="beep" ref={audioRef} src="audio\Vybz Kartel Ft Tommy Lee - Informer [Full Song] MA(MP3_128K).mp3" />
     </div>
   );
 };
