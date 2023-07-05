60852a5119f506973f36d0b9a3a5a9d9009c9a73
gerald <geraldmumo6@gmail.com>
2023-07-05 03:17:43 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Clock-timer</h1>
+      <div className="length-controls">
+        <div className="control">
+          <h2 id="break-label">Break Length</h2>
+          <button
+            id="break-decrement"
+            className="control-button"
+            
+          >
+            -
+          </button>
+          <div id="break-length" className="length">
+            
+          </div>
+          <button
+            id="break-increment"
+            className="control-button"
+            
+          >
+            +
+          </button>
+        </div>
+        <div className="control">
+          <h2 id="session-label">Session Length</h2>
+          <button
+            id="session-decrement"
+            className="control-button"
+            
+          >
+            -
+          </button>
+          <div id="session-length" className="length">
+            
+          </div>
+          <button
+            id="session-increment"
+            className="control-button"
+            
+          >
+            +
+          </button>
+        </div>
+      </div>
+      <div id="timer-label"></div>
+      <div id="time-left" >
+        
+      </div>
+      <button id="start_stop" >
+        
+      </button>
+      <button id="reset" >
+        Reset
+      </button>
+      <audio id="beep" src="beep.mp3" />
     </div>
   );
 }
